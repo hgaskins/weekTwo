d2738c4e64d359c92fa5821d0a2aa0b46badad45
hannah gaskins <hnag409@gmail.com>
2016-03-21 14:27:51 -0700

    adding test and compute and app files to this commit bc I forgot the branch addition at the beginning of this problem set

--- lab-mar-21/gulp/js/app.js
+++ lab-mar-21/gulp/js/app.js
@@ -1,6 +1,6 @@
 var pAddFiveIn  = document.getElementById('pAddFiveIn');
 var pAddFiveOut = document.getElementById('pAddFiveOut');
 
-var arg = 10;
-pAddFiveIn.textContent  = 'Calling sum() with an argument of ' + arg;
-pAddFiveOut.textContent = 'sum() returned ' + sum(3,4);
+var arg = 12;
+pAddFiveIn.textContent  = 'Calling multiply() with an argument of ' + arg;
+pAddFiveOut.textContent = 'multiply() returned ' + multiply(3,4);
