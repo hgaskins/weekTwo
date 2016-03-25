57b7dfa985c79e2c2efcf5db5540e817ea287ec7
goodwid <goodwid@gmail.com>
2016-03-25 12:08:53 -0700

    added totals slot counter

--- pizza/js/compute.js
+++ pizza/js/compute.js
@@ -26,7 +26,11 @@ function pizzaStoreConstructor(loc, pizzaRange, deliveriesRange, pizzaCount, tar
   this.minMaxPizza = pizzaRange;
   this.minMaxDeliveries = deliveriesRange;
   this.pizzaCount = 0;
-  this.numPizzaz = [];
+  this.numPizzaz = [];  // daily count per timeslot.
+  // initializing slot pizza count total array to 0;
+  for (pp = 0; pp < 18; pp++) {
+      this.numPizzaz[pp]=0;
+  }
   targetArray.push(this);
 }
 //constructor for beverton specific store
@@ -53,57 +57,57 @@ var hillsboroStore = new pizzaStoreConstructor("hillsboro", [[1,3],[1,3],[1,3],
                                 [3,9],[3,9],[3,9],
                                 [5,12],[5,12],[5,12],
                                 [6,16],[6,16],[6,16]], 0, pizzaArray);
-//
-// //constructor for the downtown specific store
-// var downtownStore = new pizzaStoreConstructor("downtown", [[0,4],[0,4],[0,4],
-//                                 [0,7],[0,7],[0,7],
-//                                 [2,15],[2,15],[2,15],
-//                                 [10,26],[10,26],[10,26],
-//                                 [8,22],[8,22],[8,22],
-//                                 [0,2],[0,2],[0,2]], [[0,4],[0,4],[0,4],
-//                                 [0,4],[0,4],[0,4],
-//                                 [1,4],[1,4],[1,4],
-//                                 [4,6],[4,6],[4,6],
-//                                 [7,15],[7,15],[7,15],
-//                                 [2,8],[2,8],[2,8]], 0, pizzaArray);
-// //constructor for the northeast specific store
-// var northeastStore = new pizzaStoreConstructor("northeast", [[0,4],[0,4],[0,4],
-//                                 [0,7],[0,7],[0,7],
-//                                 [5,15],[5,15],[5,15],
-//                                 [25,39],[25,39],[25,39],
-//                                 [22,36],[22,36],[22,26],
-//                                 [5,21],[5,21],[5,21]], [[0,4],[0,4],[0,4],
-//                                 [0,4],[0,4],[0,4],
-//                                 [0,4],[0,4],[0,4],
-//                                 [13,18],[13,18],[13,18],
-//                                 [5,22],[5,22],[5,22],
-//                                 [16,31],[16,31],[16,31]], 0, pizzaArray);
-//
-// //constructor for the clackamas specific store
-// var clackamasStore = new pizzaStoreConstructor("clackamas", [[2,7],[2,7],[2,7],
-//                                 [3,8],[3,8],[3,8],
-//                                 [1,5],[1,5],[1,5],
-//                                 [5,13],[5,13],[5,13],
-//                                 [22,41],[22,41],[22,41],
-//                                 [15,20],[15,20],[15,20]], [[3,5],[3,5],[3,5],
-//                                 [3,9],[3,9],[3,9],
-//                                 [1,4],[1,4],[1,4],
-//                                 [2,4],[2,4],[2,4],
-//                                 [15,42],[15,42],[15,42],
-//                                 [6,21],[6,21],[6,21]], 0, pizzaArray);
-//
-// //constructor for the pdxAirport specific store
-// var pdxAirportStore = new pizzaStoreConstructor("pdxAirport", [[2,7],[2,7],[2,7],
-//                                 [3,8],[3,8],[3,8],
-//                                 [1,5],[1,5],[1,5],
-//                                 [5,13],[5,13],[5,13],
-//                                 [22,41],[22,41],[22,41],
-//                                 [15,20],[15,20],[15,20]], [[3,5],[3,5],[3,5],
-//                                 [3,9],[3,9],[3,9],
-//                                 [1,4],[1,4],[1,4],
-//                                 [2,4],[2,4],[2,4],
-//                                 [15,42],[15,42],[15,42],
-//                                 [6,21],[6,21],[6,21]], 0, pizzaArray);
+
+//constructor for the downtown specific store
+var downtownStore = new pizzaStoreConstructor("downtown", [[0,4],[0,4],[0,4],
+                                [0,7],[0,7],[0,7],
+                                [2,15],[2,15],[2,15],
+                                [10,26],[10,26],[10,26],
+                                [8,22],[8,22],[8,22],
+                                [0,2],[0,2],[0,2]], [[0,4],[0,4],[0,4],
+                                [0,4],[0,4],[0,4],
+                                [1,4],[1,4],[1,4],
+                                [4,6],[4,6],[4,6],
+                                [7,15],[7,15],[7,15],
+                                [2,8],[2,8],[2,8]], 0, pizzaArray);
+//constructor for the northeast specific store
+var northeastStore = new pizzaStoreConstructor("northeast", [[0,4],[0,4],[0,4],
+                                [0,7],[0,7],[0,7],
+                                [5,15],[5,15],[5,15],
+                                [25,39],[25,39],[25,39],
+                                [22,36],[22,36],[22,26],
+                                [5,21],[5,21],[5,21]], [[0,4],[0,4],[0,4],
+                                [0,4],[0,4],[0,4],
+                                [0,4],[0,4],[0,4],
+                                [13,18],[13,18],[13,18],
+                                [5,22],[5,22],[5,22],
+                                [16,31],[16,31],[16,31]], 0, pizzaArray);
+
+//constructor for the clackamas specific store
+var clackamasStore = new pizzaStoreConstructor("clackamas", [[2,7],[2,7],[2,7],
+                                [3,8],[3,8],[3,8],
+                                [1,5],[1,5],[1,5],
+                                [5,13],[5,13],[5,13],
+                                [22,41],[22,41],[22,41],
+                                [15,20],[15,20],[15,20]], [[3,5],[3,5],[3,5],
+                                [3,9],[3,9],[3,9],
+                                [1,4],[1,4],[1,4],
+                                [2,4],[2,4],[2,4],
+                                [15,42],[15,42],[15,42],
+                                [6,21],[6,21],[6,21]], 0, pizzaArray);
+
+//constructor for the pdxAirport specific store
+var pdxAirportStore = new pizzaStoreConstructor("pdxAirport", [[2,7],[2,7],[2,7],
+                                [3,8],[3,8],[3,8],
+                                [1,5],[1,5],[1,5],
+                                [5,13],[5,13],[5,13],
+                                [22,41],[22,41],[22,41],
+                                [15,20],[15,20],[15,20]], [[3,5],[3,5],[3,5],
+                                [3,9],[3,9],[3,9],
+                                [1,4],[1,4],[1,4],
+                                [2,4],[2,4],[2,4],
+                                [15,42],[15,42],[15,42],
+                                [6,21],[6,21],[6,21]], 0, pizzaArray);
 
 //var pizzaStores and var timeStamps are universal to the tables
 //object of pizzaStores
@@ -168,7 +172,10 @@ function generateTable(storeObject) {
 
         //creating all cells
         for (var i = 0; i < 18; i++) {
+            // initializing
+
           //creates table row
+
           var row = document.createElement("tr");
           var timeCell = document.createElement("td");
           var timeCellText = document.createTextNode(timeStamps[i]);
@@ -179,6 +186,7 @@ function generateTable(storeObject) {
           var cell = document.createElement("td");
           var numberOfPizzas = randoInRange(storeObject.minMaxPizza[i][0],storeObject.minMaxPizza[i][1]);
           storeObject.pizzaCount += numberOfPizzas;
+          storeObject.numPizzaz[i] += numberOfPizzas;
           // this.numPizzaz.push(numberOfPizzas);
           console.log(storeObject.pizzaCount);
           var cellText = document.createTextNode(numberOfPizzas);
@@ -222,6 +230,20 @@ function pizzaTotalCounter () {
         totalPizzaCount += pizzaArray[tt].pizzaCount;
     }
     return totalPizzaCount;
+}
+
+function weeklyTimeSlotTotals () {
+    var results = [];
+    for (dd = 0; dd < 18; dd++) {
+        results[dd] = 0; // initializing array to zero
+    }
+    for (ww in pizzaArray) {
+        for (zz =0; zz < 18; zz++) {
+            results[zz] += pizzaArray[ww].numPizzaz[zz];
+        }
+    }
+    console.log(results);
+    return results;
 }
 
 for (var qq = 0; qq < pizzaArray.length; qq++) {
